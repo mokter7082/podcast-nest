843cecdf580d6968129e40d16481a5738b522a8c
mokter.m4yours <mokter.m4yours@gmail.com>
2023-08-27 23:51:42 +0600

    marge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AlbumModule } from './album/album.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
-import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { DirectoryModule } from './directory/directory.module';
 import { MediaModule } from './media/media.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.env.stage.${process.env.STAGE}`,
-    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
